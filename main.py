09b3094e124de4dc8d444b19f41342cf99f30cc9
Afostyrio <gonzalez.joshua@hotmail.com>
2026-01-28 18:11:57 -0600

    New structure: MusicWidget
    I've cleaned out the code a little bit by creating a new class MusicPlayer for the MusicWidget (also new). The program now runs a continuous queue of songs, but it can't be stopped.

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import urllib.request
 from textual.app import App
 from textual.widgets import Footer, Header, Static, Label, Button
 import threading
-
+import time
 
 class SubsonicError(Exception):
     """Subsonic API error occured"""
 
-class Subsonic():
+class Subsonic:
     API_VERSION = '1.15.0'
     CLIENT_NAME = 'zero-sonic'
     RESPONSE_FORMAT = 'json'
@@ -42,19 +42,24 @@ class Subsonic():
 
     def make_request(self, request):
         with self._request(request) as response:
-            body = json.loads(response.read())
-            if 'error' in body:
-                dump_json(body)
-                raise SubsonicError(
-                    '{} - {}'.format(body['error'], body['message'])
-                )
-            # To DEBUG
+            try:
+                body = json.loads(response.read())
+                if 'error' in body:
+                    dump_json(body)
+                    raise SubsonicError(
+                        '{} - {}'.format(body['error'], body['message'])
+                    )
+                # To DEBUG
+                return body
+            except:
+                print("ERROR {}".format(response.read()))
+                print(request)
+                exit()
             # dump_json(body)
-            return body
 
     @contextlib.contextmanager
     def _request(self, request):
-        r = urllib.request.Request(request, method='GET')
+        r = urllib.request.Request(request, headers={'User-Agent': 'Mozilla/6.0'}, method='GET')
         try:
             with urllib.request.urlopen(r, timeout=60) as response:
                 yield response
@@ -78,66 +83,69 @@ def dump_json(data):
 def main():
     Zerosonic().run()    
 
-class MusicPlayer(Static):
-    BINDINGS = [
-        ('space', 'play_stop', 'Plays/stops current song'),
-        ('right', 'next_track', 'Changes to the next track')
-    ]
+class MusicPlayer:
+    def __init__(self):
+        config = read_config()
+        self.server = Subsonic(url=config['url'], username=config['username'], password=config['password'])
+        request_songs = self.server.create_request("getRandomSongs", parameters={"size":500})
+        request_songs_responses = self.server.make_request(request_songs)
 
-    # Server stuff
-    config = read_config()
-    server = Subsonic(url=config['url'], username=config['username'], password=config['password'])
+        self.songs = request_songs_responses['subsonic-response']['randomSongs']['song']    
+        self.CURRENT_TRACK = 0
+        self.music_playing = None
+        self.playback_thread = threading.Thread(target=self.begin_playback_at_current_track)
 
-    # Request a random queue
-    request_songs = server.create_request("getRandomSongs", parameters={"size":500})
-    request_songs_responses = server.make_request(request_songs)
-    songs = request_songs_responses['subsonic-response']['randomSongs']['song']
-
-    CURRENT_TRACK = 0
-    playing_music = None
-
-
-    def get_info(self):
+    def get_current_track_info(self):
         request_info = self.server.make_request(self.server.create_request("getSong", parameters={'id': self.songs[self.CURRENT_TRACK]['id']}))
         info = request_info['subsonic-response']['song']
-        return Label("Now playing: {} - {}\n".format(info['title'], info['artist']), id="info-label")
+        return info['title'], info['artist']
     
-    info_label = Label()
+    def play_song(self, song_id):
+        request_play = self.server.create_request("stream", parameters={'id' : song_id})
+        self.music_playing = playsound(request_play)
 
-    def play_loop(self):
-        playsound("https://music.afostyrio.com/rest/stream.view?id=h28XCV1a3AVu2J7TEAJmzl&u=afostyrio&p=Lui0703&v=1.13.0&c=AwesomeClientName&f=xml")
-        # while self.CURRENT_TRACK <= 499:
-        request_play = self.server.create_request("stream", parameters={'id': self.songs[self.CURRENT_TRACK]['id']})
-        self.playing_music = playsound(request_play)
-        # if self.playing_music == None:
-        #     break
-        self.CURRENT_TRACK += 1
+    def begin_playback_at_current_track(self):
+        while self.CURRENT_TRACK <= 499:
+            self.play_song(self.songs[self.CURRENT_TRACK]['id'])
+            if self.music_playing is None:
+                break
+            self.CURRENT_TRACK += 1
 
-    play_thread = threading.Thread(target=play_loop)
-
-    def stop_music(self):
+    def stop_playback(self):
         try:
-            self.playing_music.stop()
-            self.playing_music = None
-            self.play_thread.join()
+            self.music_playing.stop()
+            self.music_playing = None
+            self.playback_thread.join()
         except:
-            self.playing_music = None
-    
-    def action_play_stop(self):
-        if self.playing_music is None:
-            self.play_thread.start()
-        else:
-            self.stop_music()
+            print("STOP PLAYBACK ERROR")
+            
 
-    def action_next_track(self):
-        self.stop_music()
-        self.CURRENT_TRACK += 1
-        info_label = self.get_info()
-        self.play_thread.start()
+class MusicWidget(Static):
+    def __init__(self):
+        super().__init__()
+        self.music_player = MusicPlayer()
+
+    BINDINGS = [
+        ('space', 'play_stop', 'Plays/stops the random queue playback')
+        # ('right', 'next_track', 'Changes to the next track')
+    ]
+
+    def action_play_stop(self):
+        if self.music_player.music_playing is None:
+            self.music_player.playback_thread.start()
+        else:
+            self.music_player.stop_playback()
+
+    # def action_next_track(self):
+    #     self.stop_music()
+    #     self.CURRENT_TRACK[0] += 1
+    #     info_label = self.get_info()
+    #     self.play_thread.start()
 
     def compose(self):
         yield Button("← PREV", id='prev')
-        info_label = self.get_info()
+        info = self.music_player.get_current_track_info()
+        info_label = Label("{} -- {}". format(*info), id="info-label")
         yield info_label
         yield Button("SIG →", id='sig')
 
@@ -148,17 +156,23 @@ class Zerosonic(App):
 
     CSS_PATH = 'zero-sonic.tcss'
 
-    music_player = MusicPlayer()
+    music_widget = MusicWidget()
 
     def compose(self):
         yield Header(show_clock=True)
         yield Footer()
-        yield self.music_player
+        yield self.music_widget
 
     def action_quit(self):
-        self.music_player.stop_music()
+        self.music_widget.music_player.stop_playback()
         self.exit()
 
 
 if __name__ == "__main__":
-    main()
+    mp = MusicPlayer()
+    mp.playback_thread.start()
+    print("now playing")
+    time.sleep(5)
+    mp.stop_playback()
+
+    print("playback stopped")
